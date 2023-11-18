5eae4052f469f55f76d991ed6f709efd5e4839c6
ZivoMartin <martin.zivojinovic@etu.u-bordeaux.fr>
2023-11-18 23:17:32 +0100

    Enorme deception, petit coup au moral.

--- src/system.rs
+++ src/system.rs
@@ -1,5 +1,6 @@
 use crate::text_file::TextFile;
 use crate::text_file::file_exists;
+use std::str::FromStr;
 
 #[allow(dead_code)]
 
@@ -35,7 +36,7 @@ impl System{
         let new_table_data_path = format!("text_files/data_{}", new_table_name);
         if !self.table_exist(new_table_name){
             self.nb_table += 1;
-            self.main_file.push(&format!("{} {}\n", new_table_name, new_table_path));
+            self.main_file.push(&format!("{}\n", new_table_name));
             TextFile::new(new_table_path);
             let mut new_table_data_file = TextFile::new(new_table_data_path);
             for data in arg{
@@ -110,23 +111,104 @@ impl System{
         }
     }
 
+    fn convert_with_good_type<T>(&self, type_value: &str, value: &str) -> T where T: FromStr + Default, {
+        match type_value {
+            "BOOL" => match value {
+                "true" => "1".parse().unwrap_or_default(),
+                _ => "0".parse().unwrap_or_default(),
+            },
+            "STRING" => value.parse().unwrap_or_default(),
+            _ => String::from(value).parse().unwrap_or_default(),
+        }
+    }
+
     fn delete_line(&mut self, arg: Vec<&str>){
-        let line_file_path = format!("text_files/{}_{}", arg[0], arg[1]);
+        let line_file_path = format!("text_files/{}_line_{}", arg[0], arg[1]);
         if self.table_exist(arg[0]) && file_exists(&line_file_path){
             let mut table_file = TextFile::new(format!("text_files/{}", arg[0]));
             table_file.replace(&format!("{}\n", line_file_path), "");                                                                                                                                                                                                                                                                                                      
-            let mut line_file = TextFile::new(line_file_path);
-            let text = line_file.get_text();
-            for line in text.lines(){
-                let mut s_line = String::from(line);
-                if s_line[0..1] == String::from("f") {
-                    s_line.remove(0);
-                    TextFile::new(s_line).erase();
-                }
-            }
-            line_file.erase();
+            let line_file = TextFile::new(line_file_path);
+            self.clear_line_file(line_file);
         }
     }
+
+    fn clear_line_file(&mut self, mut line_file: TextFile){
+        let text = line_file.get_text();
+        for line in text.lines(){
+            let mut s_line = String::from(line);
+            if s_line[0..1] == String::from("f") {
+                s_line.remove(0);
+                TextFile::new(s_line).erase();
+            }
+        }
+        line_file.erase();
+    }
+
+    fn delete_line_if<F>(
+        &mut self,
+        mut arg: Vec<&str>,
+        closure: F,
+    ) 
+    where
+        F: Fn(Vec<i32>, Vec<String>, Vec<bool>, Vec<f32>) -> bool,
+    {
+        let table_name = arg[0];
+        arg.remove(0);
+        if self.table_exist(table_name){
+            let mut tab_file = TextFile::new(format!("text_files/{}", table_name));
+            let mut data_file = TextFile::new(format!("text_files/data_{}", table_name));
+            let text = tab_file.get_text();
+            let mut arg_data_vect: Vec<(i32, &str)> = Vec::new();
+            for param in arg{
+                arg_data_vect.push(self.get_arg_data(&mut data_file, param));
+            }
+            for line in text.lines(){
+                let mut line_file = TextFile::new(format!("text_files/{}_line_{}", table_name, line));
+                let line_text = line_file.get_text();
+                let mut bool_tab : Vec<bool> = Vec::new();
+                let mut string_tab : Vec<String> = Vec::new();
+                let mut float_tab : Vec<f32> = Vec::new();
+                let mut int_tab : Vec<i32> = Vec::new();
+                for data in &arg_data_vect{
+                    let the_arg = self.get_good_data(line_text.split("\n").nth(data.0 as usize).unwrap().to_string());
+                    match data.1{
+                        "INT" => int_tab.push(self.convert_with_good_type::<i32>(data.1, &the_arg)),
+                        "FLOAT" => float_tab.push(self.convert_with_good_type::<f32>(data.1, &the_arg)),
+                        "BOOL" => match self.convert_with_good_type::<i32>(data.1, &the_arg){
+                            1 => bool_tab.push(true),
+                            _ => bool_tab.push(false)
+                        },
+                        _ => string_tab.push(self.convert_with_good_type::<String>(data.1, &the_arg))
+                    }
+                }
+                if closure(int_tab, string_tab, bool_tab, float_tab){
+                    self.clear_line_file(line_file);
+                }
+            }
+        }
+    }
+
+    fn get_good_data(&mut self, mut value: String) -> String{
+        let type_data = value.remove(0);
+        match type_data{
+            'f' => return TextFile::new(value).get_text(),
+            _ => return value
+        }
+    }
+
+    fn get_arg_data(&mut self, data_file: &mut TextFile, arg: &str) -> (i32, str){
+        let text = data_file.get_text();
+        let mut result = 0;
+        for line in text.lines().skip(1){
+            let mut splited_line = line.split_whitespace();
+            let name = splited_line.nth(1).unwrap();
+            if name == arg || (name == "DEFAULT" && splited_line.nth(0).unwrap() == arg){
+                return (result, line.split_whitespace().nth(0).unwrap());
+            }
+            result += 1;
+        }
+        return (-1, "");
+    }   
 
     fn delete_table(&mut self, arg: Vec<&str>){
         if self.table_exist(arg[0]){
@@ -136,6 +218,8 @@ impl System{
                 self.delete_line(vec!{arg[0], line});
             }
             tab_file.erase();
+            TextFile::new(format!("text_files/data_{}", arg[0])).erase();
+            self.main_file.replace(&format!("{}\n", arg[0]), "");
         }
     }
 }
